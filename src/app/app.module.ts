1d94426ce38a6aeaee071f40b9bebb0072788983
Carlos <carloseassis2001@gmail.com>
2024-03-17 16:43:35 -0300

    Add first route to weather-home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { WheaterHomeComponent } from './modules/wheater/page/wheater-home/wheater-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WheaterHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
